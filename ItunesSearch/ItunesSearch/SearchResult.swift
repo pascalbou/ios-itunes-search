baa74abd29bce42eaba5a03cbe4e88295fa0e3c7
krikaz <pascal.bou@protonmail.com>
2020-03-13 00:32:57 +0100

    part 1.1

---
+++ ItunesSearch/ItunesSearch/SearchResult.swift
@@ -0,0 +1,23 @@
+//
+//  SearchResult.swift
+//  ItunesSearch
+//
+//  Created by krikaz on 3/13/20.
+//  Copyright © 2020 thewire. All rights reserved.
+//
+
+import Foundation
+
+struct SearchResult: Codable {
+    var title: String
+    var creator: String
+    
+    enum CodingKeys: String, CodingKey {
+        case title = "trackName"
+        case creator = "artistName"
+    }
+}
+
+struct SearchResults: Codable {
+    let results: [SearchResult]
+}
