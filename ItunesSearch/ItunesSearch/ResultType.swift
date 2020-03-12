bfc3dfba01c12a83eee0b825427bbac78fdb0045
krikaz <pascal.bou@protonmail.com>
2020-03-13 00:34:20 +0100

    part 1.2

---
+++ ItunesSearch/ItunesSearch/ResultType.swift
@@ -0,0 +1,15 @@
+//
+//  ResultType.swift
+//  ItunesSearch
+//
+//  Created by krikaz on 3/13/20.
+//  Copyright © 2020 thewire. All rights reserved.
+//
+
+import Foundation
+
+enum ResultType: String {
+    case software
+    case musicTrack
+    case movie
+}
