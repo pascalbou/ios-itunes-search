73762ef3cfe129d22c25b4693d1b5b5bcdcba5a8
krikaz <pascal.bou@protonmail.com>
2020-03-13 01:01:51 +0100

    part 1.3

---
+++ ItunesSearch/ItunesSearch/SearchResultController.swift
@@ -0,0 +1,59 @@
+//
+//  SearchResultController.swift
+//  ItunesSearch
+//
+//  Created by krikaz on 3/13/20.
+//  Copyright © 2020 thewire. All rights reserved.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case put = "PUT"
+    case post = "POST"
+    case delete = "DELETE"
+}
+
+class SearchResultController {
+    let baseURL = URL(string: "https://itunes.apple.com")!
+    var searchResults: [SearchResult] = []
+    
+    func performSearch(searchTerm: String, resultType: ResultType, completion: @escaping (_: Error?) -> Void) {
+        
+        var urlComponents = URLComponents(url: baseURL, resolvingAgainstBaseURL: true)
+        let searchTermQueryItem = URLQueryItem(name: "search", value: searchTerm)
+        urlComponents?.queryItems = [searchTermQueryItem]
+        
+        guard let requestURL = urlComponents?.url else {
+            completion(NSError())
+            return
+        }
+        
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = HTTPMethod.get.rawValue
+        
+        URLSession.shared.dataTask(with: request) { (data, _, error) in
+            if let error = error {
+                completion(error)
+                return
+            }
+            
+            guard let data = data else {
+                completion(NSError())
+                return
+            }
+            
+            let jsonDecoder = JSONDecoder()
+            do {
+                let searchResultsJSON = try jsonDecoder.decode(SearchResults.self, from: data)
+                self.searchResults.append(contentsOf: searchResultsJSON.results)
+                completion(nil)
+            } catch {
+                completion(error)
+            }
+            
+        }.resume()
+        
+    }
+}
